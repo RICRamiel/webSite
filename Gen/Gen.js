47acc5860be95bd1bb872efc45f86d23fbe38e7b
RICRamiel <tardis40type@gmail.com>
2024-04-03 00:10:42 +0700

    genetic-algo v1

--- Gen/Gen.js
+++ Gen/Gen.js
@@ -1,269 +1,73 @@
-/**
- * Генерирует полносвязаный граф
- * @param int vertex
- * @returns {Array}
- */
-function GraphGen(vertex)
-{
-    var gr = new Array();
-
-    var t_part = []
-
-    for(var i=0; i< vertex; i++)
-    {
-        var link = Math.floor(Math.random()*(vertex-1)); // Связь с вершиной
-        var curent_vertex = i;
-        if(link === i)
-        {
-            link--;
-            if(link < 0)
-            {
-                link = vertex - 1;
-            }
-        }
-
-        if(gr[curent_vertex] === undefined)
-        {
-            gr[curent_vertex] = [];
-        }
-
-        if(gr[link] === undefined)
-        {
-            gr[link] = [];
-            t_part.push(link);
-        }
-
-
-        if(gr[curent_vertex].indexOf(link) === -1 )
-        {
-            gr[curent_vertex].push(link);
-        }
-
-        if(gr[link].indexOf(curent_vertex) === -1 )
-        {
-            gr[link].push(curent_vertex);
-        }
-
-    }
-
-    console.log(["GRAPH", gr, t_part])
-
-    for(var i=1; i< t_part.length; i++)
-    {
-        if(gr[ t_part[i] ].indexOf(t_part[i-1]) === -1 )
-        {
-            gr[ t_part[i] ].push(t_part[i-1]);
-        }
-
-        if(gr[ t_part[i-1] ].indexOf(t_part[i]) === -1 )
-        {
-            gr[ t_part[i-1] ].push(t_part[i]);
+function calculateFitness() {
+    for (var i = 0; i < popSize; i++) {
+        population[i] = shuffle(order);
+        var dist = getDistance(cities, population[i]);
+        fitness[i] = 1 / (dist + 1);
+        if (minDistance > dist) {
+            minDistance = dist;
+            bestPath = population[i];
         }
     }
-
-    if(gr[ t_part[0] ].indexOf(t_part[t_part.length-1]) === -1 )
-    {
-        gr[ t_part[0] ].push(t_part[t_part.length-1]);
-    }
-
-    if(gr[ t_part[t_part.length-1] ].indexOf(t_part[0]) === -1 )
-    {
-        gr[ t_part[t_part.length-1] ].push(t_part[0]);
-    }
-
-
-    console.log(["GRAPH", gr, t_part])
-
-    // for(var i=0; i< gr.length; i++)
-    // {
-    //     console.log("G"+i, gr[i])
-    // }
-
-    return gr
-}
-
-GA = function ()
-{
-    this.gr = GraphGen(10); // Граф и его размерность
-
-    /**
-     * Генерирует сущьности
-     * @returns {Array} entity
-     */
-    this.gen = function()
-    {
-        var entity = []
-
-        var last = this.gr[0][ Math.floor((this.gr[0].length ) * Math.random()) ]
-        do
-        {
-            entity.push( last )
-            last = this.gr[last][ Math.floor((this.gr[last].length ) * Math.random()) ]
-
-            if(entity.length > 2)
-            {
-                while(   entity[entity.length-2] === last  && this.gr[last].length > 1)
-                {
-                    last = this.gr[last][ Math.floor((this.gr[last].length ) * Math.random()) ]
-                }
-            }
-
-            if( entity.length > 10 *this.gr.length )
-            {
-                break;
-            }
-
-        }while( this.test(entity) === -1 );
-
-
-        return entity;
-    }
-
-    /**
-     * Тестирует на пригодность переданую сущьность
-     * @param array entity сущьность
-     * @returns {Number|@exp;entity@pro;length} Коэфицент пригодности, чем меньше тем лучше или -1 в случаии полной непригодности индивида.
-     */
-    this.test = function(entity)
-    {
-        //console.error(entity)
-        for(var k in this.gr)
-        {
-            if(entity.indexOf(k/1) === -1)
-            {
-                return -1; // Не прошол по всем вершинам.
-            }
-        }
-
-        return entity.length;
-    }
-
-    /**
-     * Зменяет у сущьности весе элементы начиная с start
-     * @returns {Array} entity
-     */
-    this.ReGenFrom = function(base_entity, start)
-    {
-        var entity = base_entity.slice(start-1);
-
-        var last = base_entity[start]
-        do
-        {
-            entity.push( last )
-            last = this.gr[last][ Math.floor((this.gr[last].length -1) * Math.random()) ]
-
-            if(entity.length > 2)
-            {
-                while(   entity[entity.length-2] === last  && this.gr[last].length > 1)
-                {
-                    last = this.gr[last][ Math.floor((this.gr[last].length ) * Math.random()) ]
-                }
-            }
-
-            if( entity.length > 10 *this.gr.length )
-            {
-                break;
-            }
-
-        }while( this.test(entity) === -1 );
-
-
-        return entity;
-    }
-
-    this.generation = [] // Особи поколения
-    this.generation_size = 8; // Размер поколения
-
-
-    this.getAnswer = function()
-    {
-        var k = 0;
-        var re = 999; // Хранит значение минимальной найденой длины
-        var rkn = 0;  // Хранит текущее количество итераций не приведших к улучшению ответа
-        var max_itr = 4 // Максимальное количество поколений
-        do{
-            k++;
-            if(this.generation.length > 0)
-            {
-                console.warn("Итерация:"+k+"  Длина:"+ this.test( this.generation[0] ) , this.generation )
-            }
-            else
-            {
-                console.warn("Итерация:"+k)
-            }
-
-            for(var i=this.generation.length; i < this.generation_size; i ) // Цикл генерации поколений
-            {
-                var entity = this.gen()
-
-                if( this.test(entity) !== -1)
-                {
-                    console.log("A"+i, this.test(entity))
-                    i++;
-                    this.generation.push(entity)
-                }
-            }
-
-            this.generation.sort(function(a, b){ // Сортировка сгенерированых поколений по степени их пригодности
-                return a.length  - b.length;
-            })
-
-            console.log("A.length:"+this.generation.length, this.generation)
-
-            this.generation.splice( Math.floor(this.generation.length/3 ), this.generation.length) // Отрезаем наименне адоптированных
-
-            console.log("A-B length:"+this.generation.length, this.generation)
-
-            for(var i=0; this.generation.length < this.generation_size; i ) // Цикл мутации и размножения
-            {
-                var start = Math.floor(Math.random()*(this.generation[i].length ));
-                var new_entity = this.ReGenFrom(this.generation[i], start ); // Изменение цепочки начиная с случайного элемента и до конца
-
-                if( this.test(new_entity) !== -1)
-                {
-                    console.log("B"+i, this.test(new_entity))
-                    this.generation.push(new_entity)
-                    i++;
-                }
-            }
-
-            console.log("B.length:"+this.generation.length, this.generation)
-
-            this.generation.sort(function(a, b){ // Сортировка сгенерированых поколений по степени их пригодности
-                return a.length  - b.length;
-            })
-
-            this.generation.splice( Math.floor(this.generation.length/4 ), this.generation.length) // Отрезаем наименне адоптированных
-
-            console.log("C.length:"+this.generation.length, this.generation)
-
-            this.generation.sort(function(a, b){ // Сортировка сгенерированых поколений по степени их пригодности
-                return a.length  - b.length;
-            })
-
-            if( re - this.test( this.generation[0] )  === 0 )
-            {   // Если было 4 итерации без улучшения ответа то завершить работу.
-                rkn ++
-                if(rkn > 4)
-                {
-                    console.log("Top:", this.generation[0])
-                    return this.generation;
-                }
-            }
-            else
-            {
-                rkn = 0;
-            }
-            re = this.test( this.generation[0] )  // Запоминаем лучший результат за текущее поколение
-
-        }while( k < max_itr);
-        return this.generation;
-    }
+    return bestPath;
 }
 
 
+function normalizeFitness() {
+    var sum = 0;
+    for (var i = 0; i < fitness.length; i++) {
+        sum += fitness[i];
+    }
+    var total = 0;
+    for (var i = 0; i < fitness.length; i++) {
+        fitness[i] = fitness[i] / sum;
+    }
+}
+
+function pickOne(list, prob) {
+    var i = 0;
+    var r = random(1);
+    while (r > 0) {
+        r = r - prob[i];
+        i++;
+    }
+    i--;
+    return list[i].slice();
+}
 
 
-var ga = new GA()
-answer = ga.getAnswer()
-console.log("УРА! УРА! УРА!\nTop:", answer)
+function generateNext() {
+    var newPopulation = [];
+    for (var i = 0; i < population.length; i++) {
+        var orderA = pickOne(population, fitness);
+        var orderB = pickOne(population, fitness);
+        selectedOrder = crossOver(orderA.slice(), orderB.slice());
+        mutate(selectedOrder, 0.005);
+        newPopulation[i] = population;
+    }
+
+    population = newPopulation;
+
+}
+
+function crossOver(orderA, orderB) {
+    var start = floor(random(orderA.length));
+    var end = floor(start + 1, orderA.length);
+    var result = orderA.slice(start, end);
+    for (var i = 0; i < orderB.length; i++) {
+        if (!result.includes(orderB[i])) {
+            result.push(orderB[i]);
+        }
+    }
+    return result;
+}
+
+
+function mutate(a, rate) {
+    for (var i = 0; i < totalCities; i++) {
+        if (random(1) < rate) {
+            var indexA = floor(random(totalCities));
+            var indexB = (indexA + 1) % totalCities
+            swapArr(a, indexA, indexB);
+        }
+    }
+}
--- Gen/pixelWriteCanvas.js
+++ Gen/pixelWriteCanvas.js
@@ -10,15 +10,28 @@ const drawingContext = canvas.getContext("2d");
 const mapSize = document.getElementById("mapSize");
 const mapSizeDemo = document.getElementById("mapSizeDemo");
 const makePath = document.getElementById("makePath");
+const startAlgo = document.getElementById("startAlgo");
 var size = parseInt(mapSize.value);
 mapSizeDemo.innerHTML = size;
 
+let depth = 0;
+//for GA
+const popSize = 100;
+
+let population = [];
+let fitness = [];
+let cities = [];
+var order = [];
+
+var minDistance = Infinity;
+var bestPath;
+let totalCities;
 
 //on open canvas
 var CELL_SIDE_COUNT = size;
 var cellPixelLength = canvas.width / CELL_SIDE_COUNT;
 var colorHistory = {};
-var graph = {}; //adjacency list
+//var graph = {}; //adjacency list
 // Set default color
 colorInput.value = "#000000";
 // Initialize the canvas background
@@ -30,9 +43,7 @@ drawingContext.fillRect(0, 0, canvas.width, canvas.height);
     guide.style.gridTemplateColumns = `repeat(${CELL_SIDE_COUNT}, 1fr)`;
     guide.style.gridTemplateRows = `repeat(${CELL_SIDE_COUNT}, 1fr)`;
 
-    [...Array(CELL_SIDE_COUNT ** 2)].forEach(() =>
-        guide.insertAdjacentHTML("beforeend", "<div></div>")
-    );
+    [...Array(CELL_SIDE_COUNT ** 2)].forEach(() => guide.insertAdjacentHTML("beforeend", "<div></div>"));
 }
 
 let MakingPath = 0;
@@ -59,14 +70,20 @@ mapSize.oninput = function () {
         guide.style.gridTemplateColumns = `repeat(${CELL_SIDE_COUNT}, 1fr)`;
         guide.style.gridTemplateRows = `repeat(${CELL_SIDE_COUNT}, 1fr)`;
 
-        [...Array(CELL_SIDE_COUNT ** 2)].forEach(() =>
-            guide.insertAdjacentHTML("beforeend", "<div></div>")
-        );
+        [...Array(CELL_SIDE_COUNT ** 2)].forEach(() => guide.insertAdjacentHTML("beforeend", "<div></div>"));
     }
 }
 
+
+function setup() {
+    order = [];
+    for (let i = 0; i < totalCities; i++) {
+        order[i] = i;
+    }
+    bestPath = cities.slice();
+}
+
 function handleCanvasMousedown(event) {
-    // Ensure user is using their primary mouse button
     if (event.button !== 0) {
         return;
     }
@@ -87,26 +104,34 @@ function handleCanvasMousedown(event) {
 
     } else {
         fillCell(cellX, cellY);
-        graph[`${cellX}_${cellY}`] = [];
+        cities = formateCities();
     }
+
 }
 
 function handleClearButtonClick() {
-    const yes = confirm("Are you sure you wish to clear the canvas?");
+    //const yes = confirm("Are you sure you wish to clear the canvas?");
 
-    if (!yes) return;
+    //if (!yes) return;
+    minDistance = Infinity;
     colorHistory = {};
-    graph = {};
+    cities = [];
+    population = [];
+    fitness = [];
+    depth = 0;
     drawingContext.fillStyle = "#ffffff";
     drawingContext.fillRect(0, 0, canvas.width, canvas.height);
 }
 
+function tempClear() {
+    drawingContext.fillStyle = "#ffffff";
+    drawingContext.fillRect(0, 0, canvas.width, canvas.height);
+}
 
 function handleToggleGuideChange() {
     guide.style.display = toggleGuide.checked ? null : "none";
 
 }
-
 
 function addPath(event) {
     console.log("start")
@@ -118,25 +143,25 @@ function getPos(event) {
     const canvasBoundingRect = canvas.getBoundingClientRect();
     const x = event.clientX - canvasBoundingRect.left;
     const y = event.clientY - canvasBoundingRect.top;
-    console.log(`${x}, ${y}`);
+    //console.log(`${x}, ${y}`);
     const cellX = Math.floor(x / cellPixelLength);
     const cellY = Math.floor(y / cellPixelLength);
-    console.log(`${cellX}, ${cellY}`);
+    //console.log(`${cellX}, ${cellY}`);
     if (`${cellX}_${cellY}` in colorHistory) {
         GraphData.push([cellX, cellY]);
         MakingPath += 1;
 
         if (MakingPath === 2) {
-            console.log(GraphData)
+            //console.log(GraphData)
             let pathStart = GraphData[0];
             let pathEnd = GraphData[1];
-            console.log(graph);
-            console.log(pathStart, pathEnd);
-            let dist = calcDistance(pathStart[0], pathStart[1], pathEnd[0], pathEnd[1]);
+            //console.log(graph);
+            //console.log(pathStart, pathEnd);
+            let dist = calcPointDistance(pathStart[0], pathStart[1], pathEnd[0], pathEnd[1]);
             graph[`${pathStart[0]}_${pathStart[1]}`].push([`${pathEnd[0]}_${pathEnd[1]}`, dist]);
             graph[`${pathEnd[0]}_${pathEnd[1]}`].push([`${pathStart[0]}_${pathStart[1]}`, dist]);
             drawPath(pathStart[0], pathStart[1], pathEnd[0], pathEnd[1]);
-            console.log(graph);
+            //console.log(graph);
 
             canvas.removeEventListener('mousedown', getPos);
             canvas.addEventListener('mousedown', handleCanvasMousedown);
@@ -183,17 +208,17 @@ function requestData() {
     for (let i = 0; i < keys.length; i++) {
         data.push(keys[i].split("_").map(Number));
     }
-    console.log(data);
+    //console.log(data);
     return data;
 }
 
-function drawPath(x1, y1, x2, y2) {
+function drawPath(x1, y1, x2, y2, color = "#000000", thickness = 2) {
     x1 += 0.5;
     y1 += 0.5;
     x2 += 0.5;
     y2 += 0.5;
-    drawingContext.fillStyle = "#000000";
-    drawingContext.lineWidth = 5;
+    drawingContext.strokeStyle = color;
+    drawingContext.lineWidth = thickness;
     drawingContext.beginPath();
     drawingContext.moveTo(x1 * cellPixelLength, y1 * cellPixelLength);
     drawingContext.lineTo(x2 * cellPixelLength, y2 * cellPixelLength);
@@ -201,8 +226,66 @@ function drawPath(x1, y1, x2, y2) {
     drawingContext.closePath();
 }
 
+function drawBestPath(a, pointOrder, color, thickness) {
+    for (var i = 0; i < pointOrder.length - 1; i++) {
+        drawPath(a[pointOrder[i]].x / cellPixelLength, a[pointOrder[i]].y / cellPixelLength, a[pointOrder[i + 1]].x / cellPixelLength, a[pointOrder[i + 1]].y / cellPixelLength, color, thickness);
+    }
+}
+
+function drawAllPath() {
+    let data = requestData();
+    // console.log(data);
+    for (let i = 0; i < data.length; i++) {
+        for (let j = 1; j < data.length; j++) {
+            drawPath(data[i][0], data[i][1], data[j][0], data[j][1],"black",1);
+        }
+    }
+}
+
+function formateCities() {
+    let data = requestData();
+    let cities = [];
+    for (let i = 0; i < data.length; i++) {
+        cities[i] = {x: data[i][0] * cellPixelLength, y: data[i][1] * cellPixelLength};
+    }
+    return cities;
+}
+
+function drawCities(city) {
+    tempClear();
+    for (var i = 0; i < city.length; i++) {
+        fillCell(city[i].x / cellPixelLength, city[i].y / cellPixelLength, "#000000");
+    }
+    drawAllPath();
+}
+
+function draw(event) {
+    //console.log(cities);
+    if (event.button !== 0) {
+        return;
+        return;
+    }
+
+    if (depth === 0) {
+        totalCities = cities.length;
+        setup();
+    }
+    depth += 1;
+    var bestOrder = calculateFitness();
+    normalizeFitness();
+    generateNext();
+
+    drawCities(cities);
+    drawBestPath(cities, bestOrder, "purple", 9);
+    if (depth > 0 && depth < 100) {
+        setTimeout(() => {
+            draw(event)
+        }, 200);
+    }
+}
 
 canvas.addEventListener("mousedown", handleCanvasMousedown);
 clearButton.addEventListener("click", handleClearButtonClick);
 toggleGuide.addEventListener("change", handleToggleGuideChange);
-makePath.addEventListener("click", addPath);
+makePath.addEventListener("click", drawAllPath);
+startAlgo.addEventListener("click", draw)
