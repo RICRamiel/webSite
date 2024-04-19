a9a3b9151dcdc79ea184eaa1b31da629ae605176
RICRamiel <tardis40type@gmail.com>
2024-04-20 04:15:40 +0700

    test for downloading json files

--- someShit/test.js
+++ someShit/test.js
@@ -1,7 +1,38 @@
-let x, y, s;
-x = 5;
-y = 8;
+const JSONToFile = (obj, filename) => {
+    const blob = new Blob([JSON.stringify(obj, null, 2)], {
+        type: 'application/json',
+    });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = `${filename}.json`;
+    a.click();
+    URL.revokeObjectURL(url);
+};
+let readData;
 
-s = x + y;
+async function fetchJSONData() {
+    readData = await fetch("./text.json")
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error(`HTTP error! Status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then((data) => {
+            console.log(data);
+            return data;
+        })
+        .catch((error) => {
+            console.error("Unable to fetch data:", error)
+        });
+}
 
-console.log(s);
+
+//вызываем мейн функцию через кнопку на сайте, для скачивания используем JSONToFile, чтение не омобо актуально если я правильно понимаю, заменяем readData на имя нужного объекта и после двух нажатий на кнопку, скачается нужный файл
+function main() {
+    fetchJSONData();
+    console.log(readData);
+    JSONToFile(readData, 'testJsonFile');
+}
+
