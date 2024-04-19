3fae40e53b240a892d1d6c2e37e8615e4e7714cc
Sonya <sofyakovalenko2005610@gmail.com>
2024-04-20 05:04:47 +0700

    train neuro

--- Neuro/train_neuro.js
+++ Neuro/train_neuro.js
@@ -3,64 +3,79 @@ let hiddenLayerNeuronsNumber = 350;
 let outputLayerNeuronsNumber = 10;
 let learning_rate = 0.1;
 
-function sigmoid(x){
+function sigmoid(x) {
     return (1 / (1 + Math.exp(-x)));
 }
 
-function sigmoidDerivative(x){
+function sigmoidDerivative(x) {
     return x * (1 - x);
 }
 
+function sigmoidForMTX(mtx) {
+    return mtx.map(function (elem) {
+        return elem.map(function (num) {
+            return sigmoid(num);
+        });
+    });
+}
+
+function sigmoidDerivativeForMTX(mtx) {
+    return mtx.map(function (elem) {
+        return elem.map(function (num) {
+            return sigmoidDerivative(num);
+        });
+    });
+}
+
 let hidden_weights = new Array(hiddenLayerNeuronsNumber);
-for (let i = 0; i < hiddenLayerNeuronsNumber; i++){
+for (let i = 0; i < hiddenLayerNeuronsNumber; i++) {
     hidden_weights[i] = new Array(inputLayerNeuronsNumber);
-    for (let j =0; j < inputLayerNeuronsNumber; j++){
-        hidden_weights[i][j] = Math.random()*4-3;
+    for (let j = 0; j < inputLayerNeuronsNumber; j++) {
+        hidden_weights[i][j] = Math.random() * 4 - 3;
     }
 }
-hidden_weights = hidden_weights.map(function(elem) {
-    return elem.map(function(num) {
+hidden_weights = hidden_weights.map(function (elem) {
+    return elem.map(function (num) {
         return num * Math.sqrt(2 / inputLayerNeuronsNumber);
     });
 });
 
-
 let hidden_bias = new Array(hiddenLayerNeuronsNumber);
-for (let i = 0; i < hiddenLayerNeuronsNumber; i++){
+for (let i = 0; i < hiddenLayerNeuronsNumber; i++) {
     hidden_bias[i] = [0];
 }
 
 let output_weights = new Array(outputLayerNeuronsNumber);
-for (let i = 0; i < outputLayerNeuronsNumber; i++){
+for (let i = 0; i < outputLayerNeuronsNumber; i++) {
     output_weights[i] = new Array(hiddenLayerNeuronsNumber);
-    for (let j =0; j < hiddenLayerNeuronsNumber; j++){
-        output_weights[i][j] = Math.random()*5-2;
+    for (let j = 0; j < hiddenLayerNeuronsNumber; j++) {
+        output_weights[i][j] = Math.random() * 4 - 3;
     }
 }
 
 let output_bias = new Array(outputLayerNeuronsNumber);
-for (let i = 0; i < outputLayerNeuronsNumber; i++){
+for (let i = 0; i < outputLayerNeuronsNumber; i++) {
     output_bias[i] = [0];
 }
 
-function  np_dot_0(mtx1, arr){
+function np_dot_0(mtx1, arr) {
     let res = new Array(mtx1.length);
-    for (let i = 0; i < mtx1.length;i++){
+    for (let i = 0; i < mtx1.length; i++) {
         res[i] = [0];
-        for (let k = 0; k<mtx1[0].length;k ++){
-            res[i][0] += mtx1[i][k] * (arr[k]/255);
+        for (let k = 0; k < mtx1[0].length; k++) {
+            res[i][0] += mtx1[i][k] * (arr[k] / 255);
         }
     }
     return res;
 }
 
-function np_dot(mtx1, mtx2){
+function np_dot(mtx1, mtx2) {
     let res = new Array(mtx1.length);
-    for (let i = 0; i < mtx1.length;i++){
+    for (let i = 0; i < mtx1.length; i++) {
         res[i] = new Array(mtx2[0].length);
-        for (let j = 0; j < mtx2[0].length; j ++){
+        for (let j = 0; j < mtx2[0].length; j++) {
             res[i][j] = 0;
-            for (let k = 0; k<mtx1[0].length;k ++){
+            for (let k = 0; k < mtx1[0].length; k++) {
                 res[i][j] += mtx1[i][k] * mtx2[k][j];
             }
         }
@@ -68,109 +83,91 @@ function np_dot(mtx1, mtx2){
     return res;
 }
 
-function np_sum(mtx1, mtx2){
-    for (let i = 0; i < mtx1.length; i ++){
-        for (let j =0; j < mtx1[0].length; j++){
+function np_sum(mtx1, mtx2) {
+    for (let i = 0; i < mtx1.length; i++) {
+        for (let j = 0; j < mtx1[0].length; j++) {
             mtx1[i][j] += mtx2[i][j];
         }
     }
     return mtx1;
 }
-function np_minus(mtx1, mtx2){
-    for (let i = 0; i < mtx1.length; i ++){
-        for (let j =0; j < mtx1[0].length; j++){
-            mtx1[i][j] -= mtx2[i][j];
+
+function np_sum_0(mtx1, num) {
+    for (let i = 0; i < mtx1.length; i++) {
+        for (let j = 0; j < mtx1[0].length; j++) {
+            mtx1[i][j] += num;
         }
     }
     return mtx1;
 }
 
-function sigmoidForMTX(mtx){
-    return mtx.map(function(elem) {
-        return elem.map(function(num) {
-            return sigmoid(num);
-        });
-    });
-}
-function sigmoidDerivativeForMTX(mtx){
-    return mtx.map(function(elem) {
-        return elem.map(function(num) {
-            return sigmoidDerivative(num);
-        });
-    });
+function np_minus(mtx1, mtx2) {
+    for (let i = 0; i < mtx1.length; i++) {
+        for (let j = 0; j < mtx1[0].length; j++)
+            mtx1[i][j] -= mtx2[i][j];
+    }
+    return mtx1;
 }
 
-function np_t(mtx){
+function np_t(mtx) {
     let res = new Array(mtx[0].length);
-    for (let i = 0; i < mtx[0].length; i ++){
+    for (let i = 0; i < mtx[0].length; i++) {
         res[i] = new Array(mtx.length);
-        for (let j  =0 ; j < mtx.length;j++){
-            res[i][j] = res[j][i];
+        for (let j = 0; j < mtx.length; j++) {
+            res[i][j] = mtx[j][i];
         }
     }
     return res;
 }
-function np_product(mtx1, mtx2){
-    for (let i = 0; i < mtx1.length; i ++){
-        for (let j =0; j < mtx1[0].length; j++){
-            mtx1[i][j] = mtx1[i][j]* mtx2[i][j];
+
+function np_product(mtx1, mtx2) {
+    for (let i = 0; i < mtx1.length; i++) {
+        for (let j = 0; j < mtx1[0].length; j++) {
+            mtx1[i][j] = mtx1[i][j] * mtx2[i][j];
         }
     }
     return mtx1;
 }
 
-function np_product_0(mtx1, num){
-    for (let i = 0; i < mtx1.length; i ++){
-        for (let j =0; j < mtx1[0].length; j++){
-            mtx1[i][j] = mtx1[i][j]* num;
+function np_product_0(mtx1, num) {
+    for (let i = 0; i < mtx1.length; i++) {
+        for (let j = 0; j < mtx1[0].length; j++) {
+            mtx1[i][j] *= num;
         }
     }
     return mtx1;
 }
 
-
-
-function train(inputs, desired_output){
-    let hidden_layer_in =np_sum(np_dot(hidden_weights, inputs), hidden_bias);
+function train(inputs, desired_output) {
+    let hidden_layer_in = np_sum(np_dot(hidden_weights, inputs), hidden_bias);
     let hidden_layer_out = sigmoidForMTX(hidden_layer_in);
 
     let output_layer_in = np_sum(np_dot(output_weights, hidden_layer_out), output_bias);
     let predicted_output = sigmoidForMTX(output_layer_in);
 
     let error = np_minus(desired_output, predicted_output);
+    console.log(error);
     let d_predicted_output = np_product(error, sigmoidDerivativeForMTX(predicted_output));
 
     let error_hidden_layer = np_dot(np_t(d_predicted_output), output_weights);
     let d_hidden_layer = np_product(np_t(error_hidden_layer), sigmoidDerivativeForMTX(hidden_layer_out));
 
-    output_weights = np_sum(np_product_0(np_t(np_dot(hidden_layer_out, np_t(d_predicted_output))),learning_rate));
-    output_bias = np_sum();
+    output_weights = np_sum(np_product_0(np_t(np_dot(hidden_layer_out, np_t(d_predicted_output))), learning_rate), output_weights);
+    let sm = 0;
+    for (let i in d_predicted_output)
+        sm += i * learning_rate;
+    output_bias = np_sum_0(output_bias, sm);
+
+    hidden_weights = np_sum(np_product_0(np_t(np_dot(inputs, np_t(d_hidden_layer))), learning_rate), hidden_weights);
+    sm = 0;
+    for (let i in d_hidden_layer)
+        sm += i * learning_rate;
+    hidden_bias = np_sum_0(hidden_bias, sm);
 }
 
-/*def train(self, inputs, desired_output):
-hidden_layer_in = np.dot(self.hidden_weights, inputs) + self.hidden_bias
-hidden_layer_out = sigmoid(hidden_layer_in) # сигма скрытого
 
-output_layer_in =  np.dot(self.output_weights, hidden_layer_out) + self.output_bias
-predicted_output = sigmoid(output_layer_in) # сигма выходного
-
-error = desired_output - predicted_output
-d_predicted_output = error * sigmoid_derivative(predicted_output)
-
-error_hidden_layer = d_predicted_output.T.dot(self.output_weights)
-d_hidden_layer = error_hidden_layer.T * sigmoid_derivative(hidden_layer_out)
-
-self.output_weights += hidden_layer_out.dot(d_predicted_output.T).T * self.learning_rate
-self.output_bias += np.sum(d_predicted_output, axis=0, keepdims=True) * self.learning_rate
-
-self.hidden_weights += inputs.dot(d_hidden_layer.T).T * self.learning_rate
-self.hidden_bias += np.sum(d_hidden_layer, axis=0, keepdims=True) * self.learning_rate*/
-
-
-
-
-function predict(inputs){
-    let hidden_layer_in =np_sum(np_dot(hidden_weights, inputs), hidden_bias);
+function predict(inputs) {
+    let hidden_layer_in = np_sum(np_dot(hidden_weights, inputs), hidden_bias);
     let hidden_layer_out = sigmoidForMTX(hidden_layer_in);
 
     let output_layer_in = np_sum(np_dot(output_weights, hidden_layer_out), output_bias);
@@ -182,8 +179,8 @@ function predict(inputs){
 let mnist_train;
 let mnist_test;
 
-async function setData(){
-    mnist_test = await fetch("../Neuro/mnist/mnist_handwritten_test.json")
+async function setData() {
+    mnist_test = await fetch("../neuro/mnist/mnist_handwritten_test.json")
         .then((res) => {
             if (!res.ok) {
                 throw new Error
@@ -191,11 +188,12 @@ async function setData(){
             }
             return res.json();
         })
-        .then((data) =>
-        {return data});
+        .then((data) => {
+            return data
+        });
     console.log(mnist_test[0].label);
 
-    mnist_train = await fetch("../Neuro/mnist/mnist_handwritten_train.json")
+    mnist_train = await fetch("../neuro/mnist/mnist_handwritten_train.json")
         .then((res) => {
             if (!res.ok) {
                 throw new Error
@@ -203,51 +201,54 @@ async function setData(){
             }
             return res.json();
         })
-        .then((data) =>
-        {return data});
+        .then((data) => {
+            return data
+        });
     console.log(mnist_train[0].label);
     main();
 }
 
 setData();
 
-function fromArrToMTX(arr){
-    let res = new Array(arr.length);
-    for (let i = 0; i < arr.length;i++){
-        res[i] = [arr[i]/255];
+function fromArrToMTX(arr, len) {
+    let res = new Array(len);
+    for (let i = 0; i < len; i++) {
+        res[i] = [arr[i] / 255];
     }
     return res;
 }
 
-function main(){
-    for (let i in mnist_train){
-        let inputs = fromArrToMTX(i.image);
+function main() {
+    for (let i = 0; i < 1000; i++) {
+        let inputs = fromArrToMTX(mnist_train[i].image, 784);
 
         let desired_output = new Array(10);
-        desired_output[i.label] = 1;
+        for (let k = 0; k < 10; k++) {
+            desired_output[k] = [0];
+        }
+        desired_output[mnist_train[i].label] = [1];
 
         train(inputs, desired_output);
+        console.log(1000 - i);
     }
 
-
-
     let correct_counter = 0;
-    for (let i = 0; i < 10000; i ++){
-        let prediction_list = predict(fromArrToMTX(mnist_test[i].image));
+    for (let i = 0; i < 3000; i++) {
+        let prediction_list = predict(fromArrToMTX(mnist_test[i].image, 784));
 
         let out_index = -1;
         let m = -1;
-        for (let j =0; j < 10; j++){
-            if (prediction_list[j][0] > m){
+        for (let j = 0; j < 10; j++) {
+            if (prediction_list[j][0] > m) {
                 out_index = j;
                 m = prediction_list[j][0];
             }
 
         }
         /*let out_index = prediction_list.indexOf(Math.max(...prediction_list));*/
-        console.log(out_index);
-        if (out_index === mnist_test[i].label){
-            correct_counter ++;
+        /*console.log(out_index);*/
+        if (out_index === mnist_test[i].label) {
+            correct_counter++;
         }
     }
 
